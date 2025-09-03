b2dffe6930468747fa80caecfcb76ab1c304d372
Isabela💋 <isabela.gfelix@aluno.edu.es.gov.br>
2025-09-03 11:59:13 -0300

    Create index.php

---
+++ aula 1 3trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head> <title>Aula 1 php </title> 
+    </head>
+    <body>
+        <? php 
+           echo "Olá, mundo!";
+           echo "Isabela felix e Ana julia";
+        
+        ?>
+    </body>
+</html>
