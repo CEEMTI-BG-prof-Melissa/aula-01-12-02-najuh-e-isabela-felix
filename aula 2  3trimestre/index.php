000552fc2035deed5dc6993bc13a2e0f7e229ccb
Isabela💋 <isabela.gfelix@aluno.edu.es.gov.br>
2025-09-03 12:00:06 -0300

    Create index.php

---
+++ aula 2  3trimestre/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Aula 2-php </title>
+    </head>
+    <body>
+        <?php
+             $nome1= "Isabela";
+             $nome2= "Ana julia";
+
+             echo "Meu nome é ".$nome1 ." e  minha dupla é " .$nome2;
+        ?>
+    </body>
+</html>
