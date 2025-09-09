da5fd882261c633f528d340299d36dbf3e1a0965
Isabela💋 <isabela.gfelix@aluno.edu.es.gov.br>
2025-09-09 11:56:22 -0300

    Create index.php

---
+++ aula 03-3 trimestre/index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title> Aula 03-php </title>
+    </head>
+    <body>
+        <?php
+             $nome1= "Isabela";
+             $nome2= "Ana julia";
+
+             $n1=10;
+             $n2=2;
+
+             $soma=$n1+$n2;
+
+
+             echo "Meu nome é  " .$nome1 . "  e  minha dupla  é  " .$nome2 ; 
+             echo "<br> Meu nome é $nome1 e minha dupla é $nome2.";
+
+             echo "<br><br> A soma de $n1 com $n2 é igual a $soma. ";
+        ?>
+   </body>
+</html>
