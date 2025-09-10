a424f930d7f0406a013619d1aeb3e5a2ab12b8a6
Isabela💋 <isabela.gfelix@aluno.edu.es.gov.br>
2025-09-10 11:46:12 -0300

    Update index.php

--- aula 03-3 trimestre/index.php
+++ aula 03-3 trimestre/index.php
@@ -17,6 +17,10 @@
              echo "<br> Meu nome é $nome1 e minha dupla é $nome2.";
 
              echo "<br><br> A soma de $n1 com $n2 é igual a $soma. ";
+             echo "<br><br> A subtração dos números $n1 e $n2 é igual a:  " . ($n1-$n2). ".";
+             echo "<br><br> A multiplicação dos números $n1 e $n2 é igual a: " . ($n1*$n2). ".";
+             echo "<br><br> A divisão dos números $n1 e $n2 é igual a: " . ($n1/$n2). ".";
+            echo "<br><br> A operação de módulo dos numeros $n1 e $n2 é igual a: " . ($n1%$n2). "."
         ?>
    </body>
 </html>
